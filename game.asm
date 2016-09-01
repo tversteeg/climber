fc0bb8cd112b1a6a64c42ce762f296e1ab0ee880
Thomas Versteeg <thomasversteeg@gmx.com>
2016-09-01 15:33:29 +0200

    Fix some start variables

--- game.asm
+++ game.asm
@@ -2,27 +2,33 @@ INCLUDE "header.inc"
 
 INCLUDE "constants.asm"
 
-main:
+SECTION "Start", HOME[$0150]
 	nop
-	di
+	jp main
 
-	; Set background palette
+main:
+	di	; Disable interuptions
+
+	ld	sp,$FFFF ; Initialize the stack pointer
+
+	; Set background & sprite palettes
 	ld	a,%11100100
 	ld	[rBGP],a
+	ld	[rOBP0],a
+	ld	[rOBP1],a
 
 	; Set X/Y scroll registers to 0
 	xor	a
-	ld	[rSCY],a
 	ld	[rSCX],a
+	ld	[rSCY],a
+
+	call	StopLCD
 
 	; Turn on the LCD Display
-	ld	a,%10010101 ; LCDCF_ON|LCDCF_BG8000|LCDCF_BG9800|LCDCF_BGON|LCDCF_OBJ16|LCDCF_OBJOFF
+	ld	a,%11000111 ; LCDCF_ON|LCDCF_BG8000|LCDCF_BG9800|LCDCF_BGON|LCDCF_OBJ16|LCDCF_OBJOFF
 	ld	[rLCDC],a
 
 	ei
-
-	ld	sp,$FFFF ; Initialize the stack pointer
-	call	StopLCD
 .loop:
 	halt
 	jr	.loop
